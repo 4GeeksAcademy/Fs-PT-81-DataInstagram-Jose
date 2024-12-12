9c6aabeebf46a665e3b428b96d5cbab0afade3e3
José Navarro <joseng1997@gmail.com>
2024-12-12 11:08:20 +0000

    chore: finalizar diagram

--- src/models.py
+++ src/models.py
@@ -7,31 +7,55 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Users(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(30), nullable=False, unique=True)
+    firstname = Column(String(30))
+    lastname = Column(String(30))
+    email = Column(String, nullable=False, unique=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "firstname": self.firstname,
+            "lastname": self.lastname,
+            "email": self.email,
+        }
+
+class Posts(Base):
+    __tablename__ = 'posts'   
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship('Users', back_populates='posts')
 
-    def to_dict(self):
-        return {}
+class Comments(Base):
+    __tablename__ = 'comments'  
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250), nullable=False)
+    author_id = Column(Integer, ForeignKey('users.id'))
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    user = relationship('Users', back_populates='comments')
+    post = relationship('Posts', back_populates='comments')
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    url = Column(String, nullable=False)
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    post = relationship('Posts', back_populates='media')
+
+# Relaciones inversas
+Users.posts = relationship('Posts', back_populates='user')
+Users.comments = relationship('Comments', back_populates='user')
+Posts.comments = relationship('Comments', back_populates='post')
+Posts.media = relationship('Media', back_populates='post')
 
 ## Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
 except Exception as e:
-    print("There was a problem genering the diagram")
+    print("There was a problem generating the diagram")
     raise e
